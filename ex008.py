25512ee4ecb86a88d9df708c130d9c21999ba096
Armando Abreu <38231368+armandoabreu47@users.noreply.github.com>
2022-06-02 13:52:45 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,5 @@
+m = float(input('Digite o valor em metros para ser convertido: '))
+cm = m * 100
+mm = m * 1000
+
+print('O valor em {} metros, convertido em centimetros {} e em milimetro {}'.format(m,cm,mm))
